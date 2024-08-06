369a001586f4dacfce5568dfe5ae142476f97cba
--global <ce.denis@alumno.um.edu.ar>
2024-08-05 22:26:47 -0300

    Upgrade

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import unittest
+
 from main import suma
 
 class TestSuma(unittest.TestCase):
+
     def test_suma(self):
-        self.assertAlmostEqual(suma(1,2), 3)
+        self.assertEqual(suma(1, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
